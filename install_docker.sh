e659f085e2b8956f250b9a2fdfc43a077dc91535
Pham Thanh Tam <pttam.110494@gmail.com>
2022-03-13 21:14:03 +0700

    created docker install script

---
+++ install_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# this script is only tested on ubuntu focal 20.04 (LTS)
+
+# install docker
+echo "##############################################"
+echo "############# UPDATE AND UPGRADE #############"
+echo "##############################################"
+sudo apt-get update
+sudo apt-get full-upgrade
+echo "##############################################"
+echo "############### INSTALL DOCKER ###############"
+echo "##############################################"
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ubuntu
+
+echo "##############################################"
+echo "########### INSTALL DOCKER COMPOSE ###########"
+echo "##############################################"
+
+sudo apt-get -y install python-pip
+sudo pip install docker-compose
